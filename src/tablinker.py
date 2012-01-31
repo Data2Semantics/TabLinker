af0cded72af6b310973458b5faf5200d865ec326
Rinke Hoekstra <rjhoekstr@gmail.com>
2012-01-31 15:13:47 +0100

    Added documentation

--- src/tablinker.py
+++ src/tablinker.py
@@ -1,7 +1,11 @@
 """
 Created on 19 Sep 2011
+Modified on 31 Jan 2012
 
-@author: hoekstra
+Authors:    Rinke Hoekstra, Laurens Rietveld
+Copyright:  VU University Amsterdam, 2011/2012
+License:    LGPLv3
+
 """
 from xlutils.margins import number_of_good_cols, number_of_good_rows
 from xlutils.copy import copy
@@ -13,7 +17,6 @@ import re
 from ConfigParser import SafeConfigParser
 import urllib
 import logging
-
 #set default encoding to latin-1, to avoid encode/decode errors for special chars
 #(laurens: actually don't know why encoding/decoding is not sufficient)
 #(rinke: this is a specific requirment for the xlrd and xlutils packages)
@@ -34,9 +37,15 @@ class TabLinker(object):
     QB = Namespace('http://purl.org/linked-data/cube#')
     OWL = Namespace('http://www.w3.org/2002/07/owl#')
 
-    def __init__(self, filename):
-        self.log = logging.getLogger(__name__)
-        self.log.setLevel(logging.DEBUG)
+    def __init__(self, filename, level = logging.DEBUG):
+        """TabLinker constructor
+        
+        Keyword arguments:
+        filename -- String containing the name of the current Excel file being examined
+        level -- A logging level as defined in the logging module
+        """
+        self.log = logging.getLogger("TabLinker")
+        self.log.setLevel(level)
         
         self.log.debug('Initializing Graph')
         self.initGraph()
@@ -56,15 +65,7 @@ class TabLinker(object):
         
         
     def initGraph(self):
-        """Initialize graph
-    
-        Keyword arguments:
-        string -- Scope to init graph for 
-        
-        Returns:
-        ConjunctiveGraph
-        Namespace -- Namespace for given scope
-        """
+        """Initialize the graph, set default namespaces, and add schema information"""
     
         self.graph = ConjunctiveGraph()
         
@@ -87,6 +88,7 @@ class TabLinker(object):
         self.graph.add((self.D2S['label'], RDF.type, RDF['Property']))
     
     def setScope(self, scope):
+        """Set the default namespace and base for all URIs of the current workbook"""
         self.scope = scope
         scopens = 'http://www.data2semantics.org/data/'+scope+'/'
         
@@ -109,7 +111,7 @@ class TabLinker(object):
             self.log.debug('Base for QName generator set to: {0}'.format(self.sheet_qname))
             
             self.log.debug('Starting parser')
-            self.parse()
+            self.parseSheet()
     
     ###
     #    Utility Functions
@@ -118,8 +120,8 @@ class TabLinker(object):
     def getType(self, style):
         """Get type for a given excel style. Style name must be prefixed by 'D2S'
     
-        Keyword arguments:
-        string -- Style to check type for
+        Arguments:
+        style -- Style (string) to check type for
         
         Returns:
         String -- The type of this field. In case none is found, 'unknown'
@@ -132,11 +134,14 @@ class TabLinker(object):
         return cellType
     
     def isEmpty(self, i,j):
-        """Check if cell is empty.
+        """Check whether cell is empty.
         
-        Keyword arguments:
-        i -- Column
-        j -- Row
+        Arguments:
+        i -- row
+        j -- column
+        
+        Returns:
+        True/False -- depending on whether the cell is empty
         """
         if (self.r_sheet.cell(i,j).ctype == XL_CELL_EMPTY or self.r_sheet.cell(i,j).ctype == XL_CELL_BLANK) or self.r_sheet.cell(i,j).value == '' :
             return True
@@ -144,18 +149,47 @@ class TabLinker(object):
             return False
         
     def isEmptyRow(self, i, colns):
+        """
+        Determine whether the row 'i' is empty by iterating over all its cells
+        
+        Arguments:
+        i     -- The index of the row to be checked.
+        colns -- The number of columns to be checked
+        
+        Returns:
+        true  -- if the row is empty
+        false -- if the row is not empty
+        """
         for j in range(0,colns) :
             if not self.isEmpty(i,j):
                 return False
         return True
     
-    def isEmptyColumn(self, j, rowns):
+    def isEmptyColumn(self, j, rowns ):
+        """
+        Determine whether the column 'j' is empty by iterating over all its cells
+        
+        Arguments:
+        j     -- The index of the column to be checked.
+        rowns -- The number of rows to be checked
+        
+        Returns:
+        true  -- if the column is empty
+        false -- if the column is not empty
+        """
         for i in range(0,rowns) :
             if not self.isEmpty(i,j):
                 return False
         return True
     
     def getValidRowsCols(self) :
+        """
+        Determine the number of non-empty rows and columns in the Excel sheet
+        
+        Returns:
+        rowns -- number of rows
+        colns -- number of columns
+        """
         colns = number_of_good_cols(self.r_sheet)
         rowns = number_of_good_rows(self.r_sheet)
         
@@ -169,16 +203,15 @@ class TabLinker(object):
         self.log.debug('Number of columns with content: {0}'.format(colns))
         return rowns, colns
     
-    
-    
-    
     def getQName(self, names = {}):
         """
-        Keyword arguments:
-        mixed -- Either dict of names or string of a name.
+        Create a valid QName from a string or dictionary of names
+        
+        Arguments:
+        names -- Either dictionary of names or string of a name.
         
         Returns:
-        QName -- a valid QName for the dict or string
+        qname -- a valid QName for the dictionary or string
         """
         
         if type(names) == dict :
@@ -192,10 +225,17 @@ class TabLinker(object):
         
 
             
-    
-    
-    
     def addValue(self, source_cell_value, label=None):
+        """
+        Add a "value" + optional label to the graph for a cell in the source Excel sheet. The value is typically the value stored in the source cell itself, but may also be a copy of another cell (e.g. in the case of 'idem.').
+        
+        Arguments:
+        source_cell_value -- The string representing the value of the source cell
+        label -- Optional label for the value, otherwise the value itself is copied.
+        
+        Returns:
+        source_cell_value_qname -- a valid QName for the value of the source cell
+        """
         if not label:
             label = source_cell_value
             
@@ -209,15 +249,12 @@ class TabLinker(object):
 
     
 
+
     
-#    def appendItemInDict(self, dictionary, key, value) :
-#        if key not in dictionary : 
-#            dictionary[key] = []
-#        dictionary[key].append(value)
-#        return dictionary
-    
-    def parse(self):
-        
+    def parseSheet(self):
+        """
+        Parses the currently selected sheet in the workbook, takes no arguments. Iterates over all cells in the Excel sheet and produces relevant RDF Triples. 
+        """
         self.log.info("Parsing {0} rows and {1} columns.".format(self.rowns,self.colns))
         
         self.dimcol = {}
@@ -237,7 +274,7 @@ class TabLinker(object):
                 self.log.debug("({},{}) {}/{}: \"{}\"". format(i,j,self.cellType, self.source_cell_name, self.source_cell.value))
                 
                 if (self.cellType == 'HierarchicalRowHeader') :
-                    #Always update headerlist, and always parse hierarchical row header, even if it doesn't contain data
+                    #Always update headerlist, and always parseSheet hierarchical row header, even if it doesn't contain data
                     self.updateRowHierarchy(i, j)
                     self.parseHierarchicalRowHeader(i, j)
                 
@@ -263,12 +300,11 @@ class TabLinker(object):
 
     def updateRowHierarchy(self, i, j) :
         """
-        Build up lists for hierarchical row headers
+        Build up lists for hierarchical row headers. Cells marked as hierarchical row header are often empty meaning that their intended value is stored somewhere else in the Excel sheet.
         
         Keyword arguments:
         int i -- row number
         int j -- col number
-        dict rowhierarchy -- Current build row hierarchy
         
         Returns:
         New row hierarchy dictionary
@@ -297,7 +333,9 @@ class TabLinker(object):
         return self.rowhierarchy
     
     def parseHierarchicalRowHeader(self, i, j) :
-
+        """
+        Create relevant triples for the cell marked as HierarchicalRowHeader (i, j are row and column)
+        """
         
         # Use the rowhierarchy to create a unique qname for the cell's contents, give the source_cell's original value as extra argument
         self.log.debug("Parsing HierarchicalRowHeader")
@@ -330,6 +368,9 @@ class TabLinker(object):
 
     
     def parseRowHeader(self, i, j) :
+        """
+        Create relevant triples for the cell marked as RowHeader (i, j are row and column)
+        """
         self.source_cell_value_qname = self.addValue(self.source_cell.value)
         self.graph.add((self.SCOPE[self.source_cell_qname],self.D2S['isDimension'],self.SCOPE[self.source_cell_value_qname]))
         self.graph.add((self.SCOPE[self.source_cell_value_qname],RDF.type,self.D2S['Dimension']))
@@ -345,6 +386,9 @@ class TabLinker(object):
         return
     
     def parseHeader(self, i, j) :
+        """
+        Create relevant triples for the cell marked as Header (i, j are row and column)
+        """
         self.source_cell_value_qname = self.addValue(self.source_cell.value)   
         self.graph.add((self.SCOPE[self.source_cell_qname],self.D2S['isDimension'],self.SCOPE[self.source_cell_value_qname]))
         self.graph.add((self.SCOPE[self.source_cell_value_qname],RDF.type,self.D2S['Dimension']))
@@ -354,6 +398,9 @@ class TabLinker(object):
         return
     
     def parseProperty(self, i, j) :
+        """
+        Create relevant triples for the cell marked as Property (i, j are row and column)
+        """
         self.source_cell_value_qname = self.addValue(self.source_cell.value)
         
         self.graph.add((self.SCOPE[self.source_cell_qname],self.D2S['isDimensionProperty'],self.SCOPE[self.source_cell_value_qname]))
@@ -365,6 +412,9 @@ class TabLinker(object):
         return
     
     def parseTitle(self, i, j) :
+        """
+        Create relevant triples for the cell marked as Title (i, j are row and column)
+        """
 
         self.source_cell_value_qname = self.addValue(self.source_cell.value)
         self.graph.add((self.SCOPE[self.sheet_qname], self.D2S['title'], self.SCOPE[self.source_cell_value_qname]))
@@ -374,6 +424,9 @@ class TabLinker(object):
         
         
     def parseData(self, i,j) :
+        """
+        Create relevant triples for the cell marked as Data (i, j are row and column)
+        """
 
         observation = BNode()
         
@@ -400,32 +453,57 @@ class TabLinker(object):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    """
+    Start the TabLinker for every file specified in the configuration file (../config.ini)
+    """
+    logging.basicConfig(level=logging.INFO)
     
-    logging.debug('Reading configuration file')
+    logging.info('Reading configuration file')
     
     config = SafeConfigParser()
-    config.read('../config.ini')
+    try :
+        config.read('../config.ini')
+        srcMask = config.get('paths', 'srcMask')
+        targetFolder = config.get('paths','targetFolder')
+        verbose = config.get('debug','verbose')
+        if verbose == 1 :
+            logLevel = logging.DEBUG
+        else :
+            logLevel = logging.INFO
+    except :
+        logging.error("Could not find configuration file, using default settings!")
+        srcMask = '../input/*_marked.xls'
+        targetFolder = '../output/'
+        logLevel = logging.DEBUG
+        
+    logging.basicConfig(level=logLevel)
     
-    # Open census data files
-    files = glob.glob(config.get('paths', 'srcFolder') + '*_marked.xls')
+    # Get list of annotated XLS files
+    files = glob.glob(srcMask)
+    logging.info("Found {0} files to convert.".format(len(files)))
     
     if len(files) == 0 :
-        logging.WARNING("No files found. Are you sure the path to the annotated XLS files is correct?")
-        logging.info("Path searched: " + config.get('paths', 'srcFolder'))
+        logging.error("No files found. Are you sure the path to the annotated XLS files is correct?")
+        logging.info("Path searched: " + srcMask)
+        quit()
     
     for filename in files :
+        logging.info('Starting TabLinker for {0}'.format(filename))
         
-        logging.debug('Starting TabLinker for {0}'.format(filename))
-        tl = TabLinker(filename)
+        tl = TabLinker(filename, logLevel)
         
         logging.debug('Calling linker')
         tl.doLink()
         logging.debug('Done linking')
 
-        turtleFile = config.get('paths', 'trgtFolder') + tl.scope +'.ttl'
+        turtleFile = targetFolder + tl.scope +'.ttl'
         logging.info("Serializing graph to file {}".format(turtleFile))
-        tl.graph.serialize(turtleFile, format='turtle')
+        try :
+            tl.graph.serialize(turtleFile, format='turtle')
+        except :
+            logging.error("Whoops! Something went wrong in serializing to output file")
+            logging.info(sys.exc_info())
+            
         logging.info("Done")
     
 
